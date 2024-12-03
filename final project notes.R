adf62e3196d4f2a88528e567b76becc349c3952c
Mia Weathersby <miaw06@g.ucla.edu>
2024-12-02 21:47:46 -0800

    Update final project notes.R

--- final project notes.R
+++ final project notes.R
@@ -1,29 +1,17 @@
 library(dplyr)
 library(ggplot2)
 library(readr)
-uscrimedata <- read_csv("STATS 15/FINAL PROJECT/uscrimedata.csv")
+final_sample <- read_csv('final_sample')
 
 
 # Selects all rows up to "Property Crime" variable and removes rest
-# uscrimedata <- uscrimedata %>% 
-  # select(1:which(names(uscrimedata)=='Property Crime')) %>% 
-  # filter(if_all(everything() >= 0))
+cleaned_data <- final_sample %>%
+  select(1:which(names(final_sample) == "Property Crime"))%>%
+  filter(if_all(everything(), ~ . >= 0))
+dim(final_sample)
 
-# Removes unnecessary columns. Maybe what this code ^^ was trying to do?
-# I couldn't get that code to work properly.
-uscrimedata <- uscrimedata[, !(names(uscrimedata) %in% 
-                                 c("Agency_State", "Source.Link", "Source.Type",
-                                   "Source.Method", "FBI.Population.Covered", 
-                                   "Number.of.Agencies", "Latitude", 
-                                   "Longitude", "Comment", "Last Updated"))]
-
-# This is an alternative way to deal with rows with N/A values.
-# This just gets rid of them entirely
-uscrimedata <- uscrimedata %>% 
-  filter(!is.na(`Violent Crime`) & !is.na(`Property Crime`))
-
-# This gets rid of any rows with negative values.
-uscrimedata <- uscrimedata[rowSums(uscrimedata< 0) == 0,]
+#Finding each NA value left and cleaning
+colSums(is.na(cleaned_data))
 
 # (Alternatively) To avoid getting N/A in the “Violent Crime” or 
 # “Property Crime” columns, you can create a new column that is the sum of what 
@@ -31,24 +19,23 @@ uscrimedata <- uscrimedata[rowSums(uscrimedata< 0) == 0,]
 # rid of the original “Violent Crime” and “Property Crime” columns.
 # This will help us avoid having to delete whole rows
 # just because they have N/A in that category.
-# uscrimedata <- uscrimedata %>% mutate("Total Violent Crime" = Murder + Rape + 
+# cleaned_data <- final_sample %>% mutate("Total Violent Crime" = Murder + Rape + 
 #                                         Robbery + `Aggravated Assault`)
-#uscrimedata <- uscrimedata %>% mutate("Total Property Crime" = Burglary + 
+# cleaned_data <- cleaned_data %>% mutate("Total Property Crime" = Burglary + 
 #                                        Theft + `Motor Vehicle Theft`)
 # However, in this case, we will be using the first method of getting rid rows
 # with N/A values entirely.
 
 
 # Moving on to organizing the states by region.
-names(uscrimedata$State)
-length(unique(uscrimedata$State))
+names(cleaned_data$State)
+length(unique(cleaned_data$State))
 
 # There are 39 states (including Nationwide) used in this data
 
-sum(uscrimedata$State)
-as.factor(uscrimedata$State)
+as.factor(cleaned_data$State)
 
-table(uscrimedata$State)
+table(cleaned_data$State)
 
 # This shows how many times a state shows up in the data. 
 # This shows that are actually 38 states used in the data since "Nationwide" 
@@ -62,7 +49,7 @@ table(uscrimedata$State)
 # NORTHEAST ~> PA, MD, DC, NJ, NY, CT, MA, NH, RI (9)
 # NATIONWIDE ~> Nationwide (1)
 
-length(unique(uscrimedata$Agency))
+length(unique(cleaned_data$Agency))
 
 # Creates a data frame that matches states to their regions 
 # (there was probably a faster way to do this but it’s whatever)
@@ -78,154 +65,108 @@ region_lookup <- data.frame(State = c("OR", "HI", "WA", "CA", "ID", "CO", "WY",
 )
 
 # This adds a region column to the data (after you input the code above)
-uscrimedata <- uscrimedata %>% left_join(region_lookup, by = "State")
+cleaned_data <- cleaned_data %>% left_join(region_lookup, by = "State")
 
 # Shows how many times a region appears in the data set
 # Although this does not show how many crimes each region has,
 # it can lead us to infer which region has the most crime.
-table(uscrimedata$Region)
+table(cleaned_data$Region)
 
-# Inputting the code below will generate tables for each region showing the 
-# average amount of violent & property crime each year. There are 3 columns 
-# because there are two methods of generating this data. By using the 
-# 'Total Violent Crime' column and multiplying it by 12 or using the 
-# 'Violent Crime_mvs_12' column (I think that column may be more accurate).
 
-# Average Violent and Property Crime in the West
-westviolentcrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "West") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime_1 = 12*(mean(`Total Violent Crime`, na.rm = TRUE)))
-westviolentcrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "West") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime_2 = mean(`Violent Crime_mvs_12mo`, na.rm = TRUE))
-westviolentcrime <- westviolentcrimebyyear1 %>% 
-  left_join(westviolentcrimebyyear2, by = "Year")
+# For the sake of the data visualizations, we're going to get rid of the
+# Nationwide row.
+cleaned_data <- cleaned_data[cleaned_data$Region != "N/A", ]
 
-westpropertycrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "West") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_1 = 12*(mean(`Total Property Crime`, na.rm = TRUE)))
-westpropertycrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "West") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_2 = mean(`Property Crime_mvs_12mo`, na.rm = TRUE))
-westpropertycrime <- westpropertycrimebyyear1 %>% 
-  left_join(westpropertycrimebyyear2, by = "Year")
+# Average Violent Crime
+monthly_mean_violent_crime <- cleaned_data %>% 
+  group_by(Region) %>% 
+  summarise(monthly_mean_violent_crime = mean(`Violent Crime`, na.rm = FALSE))
+yearly_mean_violent_crime <- cleaned_data %>% 
+  group_by(Region) %>% 
+  summarise(yearly_mean_violent_crime = 12*(mean(`Violent Crime`, na.rm = FALSE)))
+# Violent crime by region data visualizations
+ggplot(monthly_mean_violent_crime, aes(x = Region, 
+                                       y = monthly_mean_violent_crime,
+                                       fill = Region)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Monthly Mean Violent Crime by Region",
+    x = "Region",
+    y = "Mean Violent Crime"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
 
-westcrimemean <- westviolentcrime %>% left_join(westpropertycrime, by = "Year")
-View(westcrimemean)
+ggplot(yearly_mean_violent_crime, aes(x = Region, y = yearly_mean_violent_crime, 
+                                      fill = Region)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Yearly Mean Violent Crime by Region",
+    x = "Region",
+    y = "Mean Violent Crime"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
 
-# Southwest
-swviolentcrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "Southwest") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime_1 = 12*(mean(`Total Violent Crime`, na.rm = TRUE)))
-swviolentcrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "Southwest") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime_2 = mean(`Violent Crime_mvs_12mo`, na.rm = TRUE))
-swviolentcrime <- swviolentcrimebyyear1 %>% 
-  left_join(swviolentcrimebyyear2, by = "Year")
+# Average Property Crime
+monthly_mean_property_crime <- cleaned_data %>% 
+  group_by(Region) %>% 
+  summarise(monthly_mean_property_crime = mean(`Property Crime`, na.rm = FALSE))
+yearly_mean_property_crime <- cleaned_data %>% 
+  group_by(Region) %>% 
+  summarise(yearly_mean_property_crime = 12*(mean(`Property Crime`, na.rm = FALSE)))
+# Property Crime by region data visualization
+ggplot(monthly_mean_property_crime, aes(x = Region, 
+                                       y = monthly_mean_property_crime, 
+                                       fill = Region)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Monthly Mean Property Crime by Region",
+    x = "Region",
+    y = "Mean Property Crime"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
 
-swpropertycrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "Southwest") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_1 = 12*(mean(`Total Property Crime`, na.rm = TRUE)))
-swpropertycrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "Southwest") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_2 = mean(`Property Crime_mvs_12mo`, na.rm = TRUE))
-swpropertycrime <- swpropertycrimebyyear1 %>% 
-  left_join(swpropertycrimebyyear2, by = "Year")
+ggplot(yearly_mean_property_crime, aes(x = Region, 
+                                      y = yearly_mean_property_crime, 
+                                      fill = Region)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Yearly Mean Property Crime by Region",
+    x = "Region",
+    y = "Mean Property Crime"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
 
-swcrimemean <- swviolentcrime %>% left_join(swpropertycrime, by = "Year")
-View(swcrimemean)
+# Here's how to compare by year
+# Violent Crime
+mean_violent_crime_by_region_year <- cleaned_data %>%
+  group_by(Region, Year) %>%
+  summarize(yearly_mean_violent_crime = 12*(mean(`Violent Crime`, na.rm = FALSE)))
+ggplot(mean_violent_crime_by_region_year, aes(x = Region, y = yearly_mean_violent_crime, fill = Region)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Mean Violent Crime by Region and Year",
+    x = "Region",
+    y = "Mean Violent Crime"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  facet_wrap(~ Year)
 
-# Midwest
-mwviolentcrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "Midwest") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime_1 = 12*(mean(`Total Violent Crime`, na.rm = TRUE)))
-mwviolentcrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "Midwest") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime_2 = mean(`Violent Crime_mvs_12mo`, na.rm = TRUE))
-mwviolentcrime <- mwviolentcrimebyyear1 %>% 
-  left_join(mwviolentcrimebyyear2, by = "Year")
-
-mwpropertycrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "Midwest") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_1 = 12*(mean(`Total Property Crime`, na.rm = TRUE)))
-mwpropertycrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "Midwest") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_2 = mean(`Property Crime_mvs_12mo`, na.rm = TRUE))
-mwpropertycrime <- mwpropertycrimebyyear1 %>% 
-  left_join(mwpropertycrimebyyear2, by = "Year")
-
-mwcrimemean <- mwviolentcrime %>% left_join(mwpropertycrime, by = "Year")
-View(mwcrimemean)
-
-# Southeast
-seviolentcrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "Southeast") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime_1 = 12*(mean(`Total Violent Crime`, na.rm = TRUE)))
-seviolentcrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "Southeast") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime_2 = mean(`Violent Crime_mvs_12mo`, na.rm = TRUE))
-seviolentcrime <- seviolentcrimebyyear1 %>% 
-  left_join(seviolentcrimebyyear2, by = "Year")
-
-sepropertycrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "Southeast") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_1 = 12*(mean(`Total Property Crime`, na.rm = TRUE)))
-sepropertycrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "Southeast") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_2 = mean(`Property Crime_mvs_12mo`, na.rm = TRUE))
-sepropertycrime <- sepropertycrimebyyear1 %>% 
-  left_join(sepropertycrimebyyear2, by = "Year")
-
-secrimemean <- seviolentcrime %>% left_join(sepropertycrime, by = "Year")
-View(secrimemean)
-
-# Northeast
-neviolentcrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "Northeast") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime = 12*(mean(`Total Violent Crime`, na.rm = TRUE)))
-neviolentcrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "Northeast") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Violent_Crime = mean(`Violent Crime_mvs_12mo`, na.rm = TRUE))
-neviolentcrime <- neviolentcrimebyyear1 %>% 
-  left_join(neviolentcrimebyyear2, by = "Year")
-
-nepropertycrimebyyear1 <- uscrimedata %>% 
-  filter(Region == "Northeast") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_1 = 12*(mean(`Total Property Crime`, na.rm = TRUE)))
-nepropertycrimebyyear2 <- uscrimedata %>% 
-  filter(Region == "Northeast") %>% 
-  group_by(Year) %>% 
-  summarize(Mean_Property_Crime_2 = mean(`Property Crime_mvs_12mo`, na.rm = TRUE))
-nepropertycrime <- nepropertycrimebyyear1 %>% 
-  left_join(nepropertycrimebyyear2, by = "Year")
-
-necrimemean <- neviolentcrime %>% left_join(nepropertycrime, by = "Year")
-View(necrimemean)
-
-# ^^ This will unfortunately take up a lot in your environment T-T
-
-# But by using this, you can see how crime differs throughout the years.
-# You can compare the values starting from 2018 to 2021.
-
-# Categorizing which city has the most or least crime can be difficult because 
-# there are so many cities. But if I can find a way to visualize that, I'll put 
-# it here.
+# Property Crime
+mean_property_crime_by_region_year <- cleaned_data %>%
+  group_by(Region, Year) %>%
+  summarize(yearly_mean_property_crime = 12*(mean(`Property Crime`, na.rm = FALSE)))
+ggplot(mean_property_crime_by_region_year, aes(x = Region, y = yearly_mean_property_crime, fill = Region)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Mean Property Crime by Region and Year",
+    x = "Region",
+    y = "Mean Property Crime"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  facet_wrap(~ Year)
